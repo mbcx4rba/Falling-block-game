37fa13d45e3a0949ab5227f8ef96af092f35f98d
robert-bn <robert.brown@tuta.io>
2020-10-15 16:05:34 +0100

    Partial implementation of collision checking

--- GameState.rkt
+++ GameState.rkt
@@ -97,21 +97,33 @@
            (get-enum-value row))))
       with-rows))))
 
-;; map-2darray maps a function across a 2d-array
+;; (vector, a) -> a
+(define get-enum-2d-value get-enum-value)
+
+;; (vector, a) -> vector
+(define get-enum-2d-index car)
+
+;; map-2d maps a function across a 2darray
 ;; 2darray -> 2darray
 (define (map-2d fn 2darray)
   (let ([as-list   (2darray->list 2darray)])
     (make-2darray (map (curry map fn) as-list))))
+
+;; fold-2d folds a 2darray
+(define (fold-2d proc init 2darray)
+  (define flat1 ;; flatten a list of lists to depth-1 only
+    (curry foldl append '()))
+  (foldl proc init (flat1 (2darray->list 2darray))))
 
 ;; board (enumerated 2darray)
 (define empty-board
   (enum-2d (empty-2darray board-height board-width)))
 
 ;; 2darray a -> (Int, Int)
-(define (dimensions 2darray)
-  (if (is-2darray? 2darray)
-      (car (get-value 2darray))
-      (raise-type-error 'dimensions "2darray" 2darray)))
+(define (get-dimensions x)
+  (cond [(is-2darray? x) (car (get-value x))]
+        [(is-tetramino? x) (get-dimensions (get-mask x))]
+        [else (raise-type-error 'get-dimensions "2darray/tetramino" x)]))
 
 ;; cell / row -> bool
 (define (is-row? row)
@@ -142,13 +154,18 @@
 
 ;; 2darray a > Int -> Int -> a
 (define (2darray-ref 2darray i j)
-  (let* ((dims    (dimensions 2darray))
-         (i-max (- (get-x dims) 1))
-         (j-max (- (get-y dims) 1)))
+  (let* ([dims    (get-dimensions 2darray)]
+         [i-max (- (get-x dims) 1)]
+         [j-max (- (get-y dims) 1)])
     (cond [(> i i-max) (raise-range-error '2darray-ref "2darray" "x " i 2darray 0 i-max)]
-          [(> j j-max) (tetramino-ref (get-tetramino 'T) 1 0)(raise-range-error '2darray-ref "2darray" "y " j 2darray 0 j-max)]
+          [(> j j-max) (tetramino-ref (get-tetramino 'T) 1 0) (raise-range-error '2darray-ref "2darray" "y " j 2darray 0 j-max)]
           [else (list-ref (list-ref (cddr 2darray) i) j)]
           )))
+
+(define (2darray-vector-ref 2darray v)
+  (let ([i (get-x v)]
+        [j (get-y v)])
+    (2darray-ref 2darray i j)))
 
 ;; tetraminio -> 
 (define (tetramino-ref tetramino i j)
@@ -256,7 +273,7 @@
   (define (in-range? min max x)
     (and (<= min x) (> max x)))
   (let* ((new-2darray (get-mask tetramino))
-         (new-dims    (dimensions  new-2darray))
+         (new-dims    (get-dimensions  new-2darray))
          (i-min       (get-x loc))
          (i-max       (+ i-min (get-x new-dims)))
          (j-min       (get-y loc))
@@ -280,7 +297,7 @@
 ;; 2darray -> 2darray
 (define (rotate-2darray 2darray)
   ;; the rotated matrix has
-  (let* ([old-dims (dimensions 2darray)]
+  (let* ([old-dims (get-dimensions 2darray)]
          [n        (get-y old-dims)]
          [m        (get-x old-dims)]
          [blank    (enum-2d (empty-2darray n m))])
@@ -304,7 +321,7 @@
         [mask-2darray (get-mask tetramino)])
   (make-tetramino colour (rotate-2darray mask-2darray))))
 
-(define starting-loc (make-vector 0 0))
+(define starting-loc (make-vector 0 5))
 
 (define (update-game world-state a-key)
   (define test-collision (const #f)) ;; debug
@@ -325,8 +342,9 @@
            [loc               (second world-state)]
            [board             (third  world-state)]
            [place? (eq? player-move 'p)]
+           [proposed-loc (update-loc player-move loc)]
            [collision? (or place?
-                           (test-collision player-move loc board))]
+                           (not (can-move-to? tetramino-in-play proposed-loc board)))]
            [new-board (if collision?
                         (place-tetramino loc
                                          tetramino-in-play
@@ -334,13 +352,50 @@
                         board)]
          [new-loc (if collision?
                       starting-loc
-                      (update-loc player-move loc))]
+                      proposed-loc)]
          [new-tetramino (cond [collision? (get-tetramino (random-choice tetraminos))]
                               [rotation?  (rotate-tetramino tetramino-in-play)]
                               [else tetramino-in-play])])
     (list new-tetramino
           new-loc
           new-board)))
+
+;; tetramino -> (Int,Int) -> board -> Bool
+;; Returns false if a tetramino collides with either
+;;    1. Another tetramino
+;;    2. The "floor" of the board
+;;    3. The "walls" of the board
+
+(define (can-move-to? tetramino loc board)
+  (let* ([tetra-dims   (get-dimensions tetramino)]
+         [tetra-height (get-y tetra-dims)]
+         [tetra-width  (get-x tetra-dims)]
+         [tetra-min-x  (get-x loc)]
+         [tetra-min-y  (get-y loc)]
+         [tetra-max-x  (+ tetra-min-x tetra-width)]
+         [tetra-max-y  (+ tetra-min-y tetra-height)]
+         [board-dims   (get-dimensions board)]
+         [board-height (get-y board-dims)]
+         [board-width  (get-x board-dims)]
+         [in-floor? (or (< tetra-min-y 0)
+                        (> tetra-max-y board-height))]
+         [in-walls? (or (< tetra-min-x 0)
+                        (> tetra-max-x board-width))]
+         [overlaps-non-empty?
+          (lambda ()  (fold-2d (lambda (x y) (and x y))
+                   #t
+                   (map-2d
+                    (lambda (cell)
+                      (let ([cell-loc (add-vector loc
+                                                  (get-enum-2d-index cell))])
+                        (is-empty? (2darray-vector-ref board cell-loc))))
+                   
+                   (enum-2d (get-mask tetramino)))))])
+      (and (not in-floor?)
+           (not in-walls?)
+           (overlaps-non-empty?))))
+    
+;    (not (or (in-floor?) (in-walls?) (overlaps-non-empty?))))) ;; De Morgan's
 
 
 
@@ -349,8 +404,6 @@
 ;; board -> image
 (define (board->image board)
   ;; cell -> char
-  (define flat1 ;; flatten a list of lists to depth-1 only
-    (curry foldl append '()))
   (let* (;; grid size in pixels
         [grid-size 25]
         [image-h (* board-height grid-size)]
@@ -365,13 +418,12 @@
                            (* (get-x indices) grid-size))]
                [y-coord (+ (/ grid-size 2)
                            (* (get-y indices) grid-size))]
-               [colour  (symbol->string (get-enum-value cell))])
+               [colour  (symbol->string (get-enum-2d-value cell))])
           (place-image (square grid-size "solid" colour)
                y-coord
                x-coord
                scene))))
-    ;; need to use a fold here
-  (foldl draw-cell canvas (flat1 (2darray->list board)))))
+  (fold-2d draw-cell canvas board)))
 
 (define (draw-game game-state)
   (let ([tetramino-in-play (first  game-state)]
